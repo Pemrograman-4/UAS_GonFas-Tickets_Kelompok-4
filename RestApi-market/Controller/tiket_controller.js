e4e55a29fb5afc98586db4b7d79197823da0f9a8
Mr.Lucky <gonzalestdimas43@gmail.com>
2021-12-29 23:10:00 +0700

    Project UAS GonFas Tickets

---
+++ RestApi-market/Controller/tiket_controller.js
@@ -0,0 +1,59 @@
+/*
+Lampung Indonesia
+*/
+const Model = require("../Model/model_tiket");
+const response = require("../Config/response");
+
+exports.get = (data) =>
+  new Promise((resolve, reject) => {
+    Model.find().then((data) => {
+      if (data.length == 0) {
+        reject(response.errorResult());
+      } else {
+        resolve(
+          Object.assign(response.suksesResult("Succes"), {
+            data: data,
+          })
+        );
+      }
+    });
+  });
+
+exports.add = (data) =>
+  new Promise((resolve, reject) => {
+    Model.create(data)
+      .then(() =>
+        resolve(response.suksesResponse("Berhasil Menambah Data Tiket"))
+      )
+      .catch(() =>
+        reject(response.errorResponse("Gagal Menambah Data Tiket"))
+      );
+  });
+
+  // Delete data
+exports.delete = async (req, res) => {
+  try {
+    await Model.findOneAndDelete({ _id: req.params.id });
+    res.json(response.suksesResponse("Berhasil Menghapus Data"));
+  } catch (error) {
+    res.json(response.errorResponse("Gagal Menghapus Data"));
+  }
+};
+
+// update data
+exports.update = async (req, res) => {
+  try {
+    await Model.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        judul: req.body.judul,
+        deskripsi: req.body.deskripsi,
+        genre: req.body.genre,
+        harga: req.body.harga,
+      }
+    );
+    res.json(response.suksesResponse("Berhasil Update Data"));
+  } catch (error) {
+    res.json(response.errorResponse("Gagal Update Data"));
+  }
+};
